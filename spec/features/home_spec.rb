766777b64b5e78cc09fd73253d0590f042f1901b
kaekasui <kaekasui_github@icloud.com>
2018-05-24 16:34:41 +0900

    Change the describe to english

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 feature 'HOME', js: true do
-  scenario 'アクセスしてHOME画面が表示されること' do
+  scenario 'Connect to home page.' do
     visit root_path
     expect(page).to have_content 'welcome'
   end
