1802224c1e97255f7d209e1d00d06ea260328369
kaekasui <kaekasui_github@icloud.com>
2018-05-25 16:07:15 +0900

    Modify the spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,12 +5,16 @@ require 'rails_helper'
 feature 'LOGIN', js: true do
   scenario 'Connect to login page.' do
     visit new_session_path
-    expect(page).to have_content 'ログイン画面'
+    within '.card-header' do
+      expect(page).to have_content 'ログイン'
+    end
   end
 
   scenario 'Link to home page.' do
     visit root_path
     click_link I18n.t('menu.login')
-    expect(page).to have_content 'ログイン画面'
+    within '.card-header' do
+      expect(page).to have_content 'ログイン'
+    end
   end
 end
