a05b0aba602f71dbada0e3ab0c78198e92ae6dce
kaekasui <kaekasui_github@icloud.com>
2018-05-26 06:18:17 +0900

    Display the sidemenu image and Design the login page asterisk

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def active_class(path)
     'active' if current_page?(path)
   end
+
+  def menu_image(path)
+    style_class_name =
+      current_page?(path) ? 'active-menu-image' : 'hover-menu-image'
+    content_tag(:span, class: "#{style_class_name} float-right") {}
+  end
 end
