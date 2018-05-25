4d69daa2c061a387b8812504ac8c2408c8cfe372
kaekasui <kaekasui_github@icloud.com>
2018-05-25 17:15:10 +0900

    Add twitter login button on login page

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,11 +10,18 @@ feature 'LOGIN', js: true do
     end
   end
 
-  scenario 'Link to home page.' do
+  scenario 'Link to login page.' do
     visit root_path
     click_link I18n.t('menu.login')
     within '.card-header' do
       expect(page).to have_content 'ログイン'
+    end
+  end
+
+  scenario 'Display the login page.' do
+    visit new_session_path
+    within '.card-body' do
+      expect(page).to have_content I18n.t('button.twitter_login')
     end
   end
 end
