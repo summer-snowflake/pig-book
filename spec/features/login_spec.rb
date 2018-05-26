157543ea023b6b188c5564df6da1486e52bcfb2e
kaekasui <kaekasui_github@icloud.com>
2018-05-26 22:26:53 +0900

    Add login and logout feature spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -24,4 +24,22 @@ feature 'LOGIN', js: true do
       expect(page).to have_content I18n.t('button.twitter_login')
     end
   end
+
+  context 'there is a user' do
+    let!(:user) { create(:user, confirmed_at: Time.zone.now) }
+
+    scenario 'Connect to mypage after sign in. and Sign out.' do
+      visit new_user_session_path
+
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button I18n.t('button.login')
+
+      expect(current_path).to eq mypage_path
+
+      click_link I18n.t('menu.logout')
+
+      expect(current_path).to eq new_user_session_path
+    end
+  end
 end
--- spec/support/feature_spec_helper.rb
+++ spec/support/feature_spec_helper.rb
@@ -2,7 +2,7 @@
 
 module FeatureSpecHelper
   def sign_in(user)
-    user.confirmed_at = Time.now
+    user.confirmed_at = Time.zone.now
     user.save
     login_as(user, scope: :user)
     visit mypage_path
