012d76b40f432801484bbfc41abb81d9908c478d
kaekasui <kaekasui_github@icloud.com>
2018-06-08 03:18:33 +0900

    Add validation to place name

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Place, type: :model do
   describe 'relationship' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
+  end
 end
