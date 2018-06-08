0074399c7c270a8d35441cc155e5fa5f2ddaaeb1
kaekasui <kaekasui_github@icloud.com>
2018-06-09 00:34:05 +0900

    Fix following rubocop rules

--- app/models/concerns/validation_error_messages_builder.rb
+++ app/models/concerns/validation_error_messages_builder.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module ValidationErrorMessagesBuilder
   extend ActiveSupport::Concern
 
   def errors_full_messages_with_keys
-    with_keys_hash = Hash.new
+    with_keys_hash = {}
     errors.keys.each do |key|
       with_keys_hash[key] = errors.full_messages_for(key)
     end
--- app/views/application/validation_error.json.jbuilder
+++ app/views/application/validation_error.json.jbuilder
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-json.error_messages @resource.errors_full_messages_with_keys if @resource.errors.present?
+if @resource.errors.present?
+  json.error_messages @resource.errors_full_messages_with_keys
+end
