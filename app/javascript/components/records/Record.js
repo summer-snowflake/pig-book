c1ecb615d9d5ae3aa6a6fb0c073e37c1cd2b21b5
kaekasui <kaekasui_github@icloud.com>
2018-06-17 23:22:00 +0900

    Change icon

--- app/javascript/components/records/Record.js
+++ app/javascript/components/records/Record.js
@@ -17,18 +17,18 @@ class Record extends React.Component {
     return (
       <tr className='record-component' id={'record-' + this.props.record.id}>
         <td>
-          <i className='far fa-dot-circle left-icon yellow' />
+          <i className='fas fa-th-large left-icon yellow' />
           {this.props.record.category_name}
         </td>
         <td>
           {this.props.record.breakdown_name ? (
-            <i className='far fa-dot-circle left-icon light-blue' />
+            <i className='fas fa-list left-icon light-blue' />
           ) : (null)}
           {this.props.record.breakdown_name}
         </td>
         <td>
           {this.props.record.place_name ? (
-            <i className='far fa-dot-circle left-icon blue' />
+            <i className='fas fa-map-marker-alt left-icon blue' />
           ) : (null)}
           {this.props.record.place_name}
         </td>
