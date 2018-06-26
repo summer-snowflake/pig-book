2b712749f617432965bece7b4f3ff6d59ecc1010
kaekasui <kaekasui_github@icloud.com>
2018-06-27 04:07:31 +0900

    Design for the record's plus or minus

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class RecordSerializer < ActiveModel::Serializer
-  attributes :id, :category_id, :category_name, :breakdown_id, :breakdown_name,
-             :place_id, :place_name, :published_at,
-             :charge, :human_charge, :memo
+  attributes :id, :category_id, :balance_of_payments, :category_name,
+             :breakdown_id, :breakdown_name, :place_id, :place_name,
+             :published_at, :charge, :human_charge, :memo
+
+  def balance_of_payments
+    object.category.balance_of_payments
+  end
 
   def category_name
     object.category.name
