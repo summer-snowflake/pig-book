f165b8dd696951cb30fa1c1de6174e05d11719ea
kaekasui <kaekasui_github@icloud.com>
2018-06-27 03:00:05 +0900

    Fix the records date

--- app/models/record/fetcher.rb
+++ app/models/record/fetcher.rb
@@ -16,7 +16,8 @@ class Record::Fetcher
     if generate_date_range
       @records = @records.where(published_at: generate_date_range)
     end
-    @records.order(created_at: :desc)
+    @records.includes(:category, :place, :breakdown)
+            .order(created_at: :desc)
   end
 
   private
