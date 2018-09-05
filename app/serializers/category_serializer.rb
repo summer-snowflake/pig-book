4e861d36bd63b9c8fcafc9b809c8f2199cc23119
kaekasui <kaekasui_github@icloud.com>
2018-09-05 14:43:00 +0900

    Add api to get recently used categories

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :human_balance_of_payments,
+  attributes :id, :name, :balance_of_payments, :human_balance_of_payments,
              :success_or_danger_style_class
 
   has_many :places, serializer: PlaceSerializer,
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
 
   namespace :api, format: :json do
     resource :base_setting, only: %w[show]
+    resources :recently_used_categories, only: %w[index]
     resources :categories, only: %w[index create update destroy] do
       resources :breakdowns, only: %w[index], module: :categories
     end
