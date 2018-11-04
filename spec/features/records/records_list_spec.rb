d0f65e41a586203da5325c5203f785583e6b6782
kaekasui <kaekasui_github@icloud.com>
2018-11-05 00:37:54 +0900

    Add feature spec to display records

--- spec/features/records/records_list_spec.rb
+++ spec/features/records/records_list_spec.rb
@@ -24,6 +24,34 @@ feature 'RECORDS', js: true do
     end
   end
 
+  context 'There are some records.' do
+    let!(:category) { create(:category, user: user) }
+    let!(:record1) do
+      create(:record,
+             user: user, category: category,
+             published_at: Time.zone.today)
+    end
+    let!(:record2) do
+      create(:record,
+             user: user,
+             published_at: Time.zone.today.beginning_of_month)
+    end
+
+    scenario 'Display the records of the current month.' do
+      visit records_path
+      within '.records-component' do
+        within all('.record-component')[0] do
+          expect(page).to have_content category.name
+          expect(page).to have_content record1.breakdown.name
+        end
+        within all('.record-component')[1] do
+          expect(page).to have_no_content category.name
+          expect(page).to have_content record2.breakdown.name
+        end
+      end
+    end
+  end
+
   after do
     logout(:user)
   end
