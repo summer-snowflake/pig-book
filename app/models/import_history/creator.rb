9fa1f5bc9fc2f72739fceec27522696dbfe6f3c8
kaekasui <kaekasui_github@icloud.com>
2019-02-14 14:53:29 +0900

    Fix the method of check_import_history

--- app/models/import_history/creator.rb
+++ app/models/import_history/creator.rb
@@ -12,11 +12,11 @@ class ImportHistory::Creator
   end
 
   def create_record
-    check_import_history
+    record_validator = check_import_history(@import_history)
     return false if errors.messages.present?
 
     ActiveRecord::Base.transaction do
-      record = @user.records.new(@record_validator.params)
+      record = @user.records.new(record_validator.params)
       @import_history.update!(record_id: record.id) if record.save
       true
     rescue StandardError => e
@@ -68,16 +68,16 @@ class ImportHistory::Creator
     generate_color_code
   end
 
-  def check_import_history
-    errors.add(:record, :registered) && return if @import_history.registered?
-    @record_validator = ImportHistory::RecordValidator.new(
-      user: @user,
-      row: @import_history.row.split(',')
+  def check_import_history(import_history)
+    errors.add(:record, :registered) && return if import_history.registered?
+    record_validator = ImportHistory::RecordValidator.new(
+      user: @user, row: import_history.row.split(',')
     )
-    if @record_validator.invalid?
-      @record_validator.errors.each do |key, msg|
+    if record_validator.invalid?
+      record_validator.errors.each do |key, msg|
         errors.add(key, msg)
       end
-    end && return
+    end
+    record_validator
   end
 end
