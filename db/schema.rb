bcc7dd4dda38e548c67c3175cc789f7ab0d3d766
kaekasui <kaekasui_github@icloud.com>
2019-03-09 02:43:39 +0900

    Add table download_files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_154049) do
+ActiveRecord::Schema.define(version: 2019_03_08_173849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2019_02_23_154049) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_categorized_places_on_category_id"
     t.index ["place_id"], name: "index_categorized_places_on_place_id"
+  end
+
+  create_table "download_files", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "filename", null: false
+    t.string "path", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_download_files_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -201,6 +210,7 @@ ActiveRecord::Schema.define(version: 2019_02_23_154049) do
   add_foreign_key "categories", "users"
   add_foreign_key "categorized_places", "categories"
   add_foreign_key "categorized_places", "places"
+  add_foreign_key "download_files", "users"
   add_foreign_key "events", "users"
   add_foreign_key "import_histories", "records"
   add_foreign_key "import_histories", "users"
