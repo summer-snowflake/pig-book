ce25ce6e448197cdf6cd5627fbbb673777335028
mkojima <kaekasui_github@icloud.com>
2019-10-31 00:05:12 +0900

    Fix button colors

--- app/front/javascripts/common/SubmitButton.js
+++ app/front/javascripts/common/SubmitButton.js
@@ -14,7 +14,7 @@ class SubmitButton extends React.Component {
   render() {
     return (
       <span className='submit-button-component'>
-        <button className='btn btn-secondary' disabled={this.props.isDisabled} id='submit' onClick={this.handleClickButton}>
+        <button className='btn btn-warning' disabled={this.props.isDisabled} id='submit' onClick={this.handleClickButton}>
           {'設定する'}
         </button>
       </span>
