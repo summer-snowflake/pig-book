4267aeddaf058ac1f8b386bc8a9863452c3b58d8
mkojima <kaekasui_github@icloud.com>
2020-02-09 02:41:22 +0900

    Add header and the styles, images

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 
-import './stylesheets/bootstrap.min.css';
+import 'stylesheets/bootstrap.min.css';
+import 'stylesheets/common.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
