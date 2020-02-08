0e6f478b5f1b49bd10290aa7ef79db7d59469c63
mkojima <kaekasui_github@icloud.com>
2020-02-09 00:06:09 +0900

    Modify the BrowserRouter range

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,23 +1,15 @@
 import React, { Component} from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-
-import TopPage from 'components/topPage';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <Router>
-        <header>
-          <ul>
-            <li><Link to='/'>HOME</Link></li>
-            <li><Link to='/users/sign_in'>ログイン</Link></li>
-          </ul>
-        </header>
-
-        <div>
-          <Route path='/' exact component={TopPage} />
-        </div>
-      </Router>
+      <header>
+        <ul>
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/users/sign_in'>ログイン</Link></li>
+        </ul>
+      </header>
     );
   }
 }
