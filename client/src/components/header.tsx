291141fecec43916d96d6a6093b5c56679edb580
mkojima <kaekasui_github@icloud.com>
2020-02-08 22:11:14 +0900

    Remove unnecessary menus now

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -10,12 +10,6 @@ class Header extends Component {
         <header>
           <ul>
             <li><Link to='/'>HOME</Link></li>
-            <li><Link to='/records/new'>入力する</Link></li>
-            <li><Link to='/records'>リスト</Link></li>
-            <li><Link to='/dashboard'>ダッシュボード</Link></li>
-            <li><Link to='/admin'>管理</Link></li>
-            <li><Link to='/base_setting'>各種設定</Link></li>
-            <li><Link to='/mypage'>マイページ</Link></li>
             <li><Link to='/users/sign_in'>ログイン</Link></li>
           </ul>
         </header>
