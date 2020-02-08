b3ec18f798d774ad6b03907dc17ef130a731540b
mkojima <kaekasui_github@icloud.com>
2020-02-09 02:41:22 +0900

    Add header and the styles, images

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,14 +1,25 @@
 import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
 
+import 'stylesheets/header.sass';
+import brandImage from 'images/pig.gif';
+
 class Header extends Component {
   render() {
     return (
-      <header>
-        <ul>
-          <li><Link to='/'>HOME</Link></li>
-          <li><Link to='/users/sign_in'>ログイン</Link></li>
-        </ul>
+      <header className='header'>
+        <nav className='nav'>
+          <Link className='nav-brand nav-link' to='/'>
+            <img alt='おこづかいちょうβ' className='brand-image' src={brandImage} />
+            <span className='brand-name'>おこづかいちょうβ</span>
+          </Link>
+          <ul className='nav'>
+            <li className='nav-item'><Link className='nav-link' to='/'>HOME</Link></li>
+          </ul>
+          <ul className='nav justify-content-end'>
+            <li className='nav-item'><Link className='nav-link' to='/users/sign_in'>ログイン</Link></li>
+          </ul>
+        </nav>
       </header>
     );
   }
