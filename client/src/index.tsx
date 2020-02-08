a11006bd169442e47298ead3b08fd573b687ecde
mkojima <kaekasui_github@icloud.com>
2020-02-08 22:47:01 +0900

    Add bootstrap for styles, and 'yarn add bootstrap.native'

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 
+import './stylesheets/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
