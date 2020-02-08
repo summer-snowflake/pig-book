4a8e3226748fd46d7ba6b7e3af863b0862cf0347
mkojima <kaekasui_github@icloud.com>
2020-02-09 04:37:39 +0900

    Add footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,9 +1,15 @@
 import React, { Component} from 'react';
 
+import 'stylesheets/footer.sass';
+
 class Footer extends Component {
   render() {
     return (
-      <footer>footer</footer>
+      <footer className='footer'>
+        <span className='copyright'>
+          Copyright © 2004-2020 @kae_kasui All Rights Reserved.
+        </span>
+      </footer>
     );
   }
 }
