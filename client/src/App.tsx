3b87139cfe15820f8eee85e225bf4513d32337dc
mkojima <kaekasui_github@icloud.com>
2020-02-09 00:06:09 +0900

    Modify the BrowserRouter range

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from 'components/header';
+import TopPage from 'components/topPage';
 import Footer from 'components/footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Router>
+          <Header />
+          <Route path='/' exact component={TopPage} />
+        </Router>
         <Footer />
       </div>
     );
