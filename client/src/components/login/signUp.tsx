0852834bc9200d346ec5e5292ef54b28b92a3ad8
mkojima <kaekasui_github@icloud.com>
2020-02-10 14:30:55 +0900

    Set the directory for client

---
+++ client/src/components/login/signUp.tsx
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+class signUp extends Component {
+  render() {
+    return (
+      <div className='sign-up-component'>sign up</div>
+    );
+  }
+}
+
+export default signUp;
