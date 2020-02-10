1b71f62a24d8c543cda8b67abc757c99cfec0183
mkojima <kaekasui_github@icloud.com>
2020-02-10 18:14:22 +0900

    Display the account menu on the left

--- client/src/components/login/loginMenu.tsx
+++ client/src/components/login/loginMenu.tsx
@@ -1,9 +1,21 @@
 import React, { Component} from 'react';
+import { NavLink } from 'react-router-dom';
+
+import 'stylesheets/menu.sass';
 
 class loginMenu extends Component {
   render() {
     return (
-      <div className='login-menu-component'>login menu</div>
+      <div className='login-menu-component list-group'>
+        <NavLink activeClassName='active-link-menu' className='list-group-item' to='/users/sign_in'>
+          <i className='fa fa-leaf left-icon' />
+          ログイン
+        </NavLink>
+        <NavLink activeClassName='active-link-menu' className='list-group-item' to='/users/sign_up'>
+          <i className='fa fa-heart left-icon' />
+          アカウント登録
+        </NavLink>
+      </div>
     );
   }
 }
