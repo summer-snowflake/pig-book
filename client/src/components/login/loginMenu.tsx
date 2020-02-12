bd18effb5ba24f74e019a323dcd2ae155e5b09c4
mkojima <kaekasui_github@icloud.com>
2020-02-13 02:23:29 +0900

    Set the locales for the menu of login

--- client/src/components/login/loginMenu.tsx
+++ client/src/components/login/loginMenu.tsx
@@ -1,23 +1,26 @@
 import React, { Component} from 'react';
 import { NavLink } from 'react-router-dom';
+import { withTranslation } from 'react-i18next'
 
 import 'stylesheets/menu.sass';
 
-class loginMenu extends Component {
+class loginMenu extends Component<i18nProps> {
   render() {
+    const { t } = this.props;
+
     return (
       <div className='login-menu-component list-group'>
         <NavLink activeClassName='active-link-menu' className='list-group-item' to='/users/sign_in'>
           <i className='fa fa-leaf left-icon' />
-          ログイン
+          {t('menu.login')}
         </NavLink>
         <NavLink activeClassName='active-link-menu' className='list-group-item' to='/users/sign_up'>
           <i className='fa fa-heart left-icon' />
-          アカウント登録
+          {t('menu.sign_up')}
         </NavLink>
       </div>
     );
   }
 }
 
-export default loginMenu;
+export default withTranslation()(loginMenu);
