20bc8df5e76684f1c5d5600683473ce44c052b34
mkojima <kaekasui_github@icloud.com>
2020-02-13 01:38:23 +0900

    Set the i18next settings and Use translation locale files

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { withTranslation } from 'react-i18next';
 
 import Header from 'components/header';
 import TopPage from 'components/top/topPage';
 import LoginPage from 'components/login/loginPage';
 import Footer from 'components/footer';
 
-class App extends Component {
+interface i18nProps {
+  t: any
+}
+
+class App extends Component<i18nProps> {
+  constructor(props: i18nProps) {
+    super(props);
+  }
+
   render() {
     return (
       <div className='app-component'>
@@ -23,4 +32,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withTranslation()(App);
