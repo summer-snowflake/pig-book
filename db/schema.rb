d7eca1124730e9e97b1cc05cb7a06097e4a5c484
mkojima <kaekasui_github@icloud.com>
2020-02-16 03:17:24 +0900

    Add columns and settings for devise_token_auth

--- db/schema.rb
+++ db/schema.rb
@@ -181,9 +181,13 @@ ActiveRecord::Schema.define(version: 2020_03_25_074737) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "authentication_token"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.text "tokens"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
