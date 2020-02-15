90166936b7332aac3c9ea4940ee1cad311b283e4
mkojima <kaekasui_github@icloud.com>
2020-02-15 22:29:35 +0900

    Add page of 404 and Add styles

--- client/src/components/errors/Page404.tsx
+++ client/src/components/errors/Page404.tsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { withTranslation } from 'react-i18next';
 
-class Page404 extends Component {
+import 'stylesheets/errors.sass';
+
+import pigImage from 'images/pig-footprints-both.gif'
+
+class Page404 extends Component<i18nProps> {
   render() {
     return (
-      <div className='page-404-component'>
-        {'404'}
+      <div className='page-404-component container'>
+        <div className='status-code'>
+          404
+        </div>
+        <blockquote className='error-message'>
+          お探しのページは見つかりませんでした。
+        </blockquote>
       </div>
     );
   }
 }
 
-export default Page404;
+export default withTranslation()(Page404);
