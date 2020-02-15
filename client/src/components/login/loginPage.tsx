9ed742dd63e656517eb9a2b854864d9f1953218b
mkojima <kaekasui_github@icloud.com>
2020-02-15 09:16:36 +0900

    Set d-lg-none for sidemenu

--- client/src/components/login/loginPage.tsx
+++ client/src/components/login/loginPage.tsx
@@ -11,8 +11,10 @@ class LoginPage extends Component {
       <div className="login-page-component container">
         <Router>
           <div className='row'>
-            <div className='col-3'>
+            <div className='col-3 d-none d-lg-block'>
               <LoginMenu />
+            </div>
+            <div className='col-1 d-lg-none'>
             </div>
             <div className='col'>
               <Route path='/users/sign_in' exact component={SignIn} />
