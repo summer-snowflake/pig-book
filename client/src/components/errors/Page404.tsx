95b5c457678e2024f3897e6640492f6a97360aed
mkojima <kaekasui_github@icloud.com>
2020-02-15 20:48:59 +0900

    Add page404

---
+++ client/src/components/errors/Page404.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Page404 extends Component {
+  render() {
+    return (
+      <div className='page-404-component'>
+        {'404'}
+      </div>
+    );
+  }
+}
+
+export default Page404;
