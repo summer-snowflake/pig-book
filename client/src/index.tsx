f8143563e7734103426e3bc9310efac7d6913055
mkojima <kaekasui_github@icloud.com>
2020-02-18 05:08:03 +0900

    Add container, reducer, action, and components using redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from 'components/app';
+import rootReducer from 'reducers/rootReducer'
 
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
@@ -11,4 +15,20 @@ import 'stylesheets/common.sass';
 
 import 'plugins/i18n'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface ExtendedWindow extends Window {
+  [x: string]: any;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+}
+declare var window: ExtendedWindow;
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
