63942ce32a33dbb699bc3c01f4f18a09967cf18c
mkojima <kaekasui_github@icloud.com>
2020-02-18 23:55:12 +0900

    Use login instead of loginRequest

--- client/src/actions/sessionActions.jsx
+++ client/src/actions/sessionActions.jsx
@@ -6,3 +6,19 @@ export const loginRequest = () => {
     type: actionTypes.LOGIN_REQUEST
   }
 };
+
+export const login = () => {
+  return async (dispatch) => {
+    dispatch(loginRequest())
+    axios.defaults.headers.post['Content-Type'] = 'application/json'
+    axios.defaults.baseURL = 'http://localhost:3001';
+    try {
+      const res = await axios.post('/api/v2/auth/sign_in');
+      console.log('abc');
+    }
+    catch(err) {
+      console.log(err);
+      console.log('error');
+    }
+  }
+}
