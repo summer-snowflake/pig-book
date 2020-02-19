2fdf42947570ac0248ac6e494db5700f5b938d2b
mkojima <kaekasui_github@icloud.com>
2020-02-20 06:48:15 +0900

    Add cross domain settings

---
+++ server/app/controllers/options_request_controller.rb
@@ -0,0 +1,16 @@
+class OptionsRequestController < ApplicationController
+  ACCESS_CONTROL_ALLOW_METHODS = %i[GET OPTIONS].freeze
+  ACCESS_CONTROL_ALLOW_HEADERS = %i[Accept Origin Content-Type Authorization].freeze
+
+  def preflight
+    set_preflight_headers!
+    head :ok
+  end
+
+  private
+
+  def set_preflight_headers!
+    response.headers['Access-Control-Allow-Headers'] = ACCESS_CONTROL_ALLOW_HEADERS.join(',')
+    response.headers['Access-Control-Allow-Methods'] = ACCESS_CONTROL_ALLOW_METHODS.join(',')
+  end
+end
--- server/config/application.rb
+++ server/config/application.rb
@@ -33,5 +33,11 @@ module Server
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => 'http://localhost:4020',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
