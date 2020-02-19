4695cfbdcd6bd5f79a9063a48c537cf3a8eef92e
mkojima <kaekasui_github@icloud.com>
2020-02-20 02:43:42 +0900

    Add requests spec for login

--- server/db/migrate/20200219053507_devise_token_auth_create_users.rb
+++ server/db/migrate/20200219053507_devise_token_auth_create_users.rb
@@ -24,9 +24,9 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
       ## User Info
       t.string :name
@@ -37,6 +37,13 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       ## Tokens
       t.json :tokens
 
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string  :current_sign_in_ip
+      t.string  :last_sign_in_ip
+
       t.timestamps
     end
 
@@ -44,6 +51,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,       unique: true
+    add_index :users, :unlock_token,       unique: true
   end
 end
