b669b459f068668b692ef0e4c2be7060c6a3a569
mkojima <kaekasui_github@icloud.com>
2020-02-19 21:40:34 +0900

    Add model user_spec

---
+++ server/spec/factories/users.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { password }
+    authentication_token { SecureRandom.urlsafe_base64(24) }
+    provider { 'email' }
+    uid { email }
+
+    trait :active do
+      confirmed_at { Time.zone.now }
+    end
+  end
+end
