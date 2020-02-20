325fca5d21472a25cf5b148cf669a86694c34e64
mkojima <kaekasui_github@icloud.com>
2020-02-20 09:07:28 +0900

    Login with email and password and redirect to root page

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Header from 'components/header';
+import Header from 'components/headerContainer';
 import TopPage from 'components/top/topPage';
 import LoginPage from 'components/login/loginPage';
 import Footer from 'components/footer';
