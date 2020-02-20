d1a6e9fb5bc098a3e4cee108c0d6474a4e3106bb
mkojima <kaekasui_github@icloud.com>
2020-02-20 22:06:36 +0900

    Get user status of cookie

--- client/src/reducers/rootReducer.tsx
+++ client/src/reducers/rootReducer.tsx
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import sessionReducer from 'reducers/sessionReducer'
+import userStatusReducer from 'reducers/userStatusReducer'
 
 const rootReducer = combineReducers({
-  session: sessionReducer
+  session: sessionReducer,
+  loginStatus: userStatusReducer
 })
 
 export default rootReducer;
