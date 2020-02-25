94d163d893d0e89a4e8272ed836ff993400bb7a8
mkojima <kaekasui_github@icloud.com>
2020-02-26 01:56:12 +0900

    Add api to update profile

--- server/app/controllers/profiles_controller.rb
+++ server/app/controllers/profiles_controller.rb
@@ -8,9 +8,22 @@ class ProfilesController < ApplicationController
     render json: @profile, status: :ok
   end
 
+  def update
+    if @profile.update(profile_params)
+      I18n.locale = @profile.locale
+      render json: @profile, status: :ok
+    else
+      render render_validation_error
+    end
+  end
+
   private
 
   def set_profile
     @profile = current_user.profile || current_user.build_profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:locale, :currency, :memo)
   end
 end
