2008fab153cf1ea452a8f73d6a9e0657a40026e3
mkojima <kaekasui_github@icloud.com>
2020-02-29 07:12:13 +0900

    Add page for category settings

---
+++ client/src/components/settings/category/categorySettings.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CategorySettings extends Component {
+  render() {
+    return (
+      <div className='category-settings-component'>
+      </div>
+    );
+  }
+}
+
+export default CategorySettings;
