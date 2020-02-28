e26dcb0788942b141d8917b400a4e3ea6522ac9d
mkojima <kaekasui_github@icloud.com>
2020-02-28 13:05:56 +0900

    Add cookies const function

---
+++ client/src/config/axios.tsx
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL = 'http://localhost:' + SERVER_PORT
+
+export const setting = axios
