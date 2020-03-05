7d311bad6d5fd33fe38554efee16c0957dfa687e
mkojima <kaekasui_github@icloud.com>
2020-03-06 06:37:40 +0900

    Add api to create and update breakdown

--- server/app/models/breakdown.rb
+++ server/app/models/breakdown.rb
@@ -4,5 +4,6 @@ class Breakdown < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 },
+                   uniqueness: { scope: %i[user category] }
 end
