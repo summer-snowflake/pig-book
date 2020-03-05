1380867fac524ab9866f70cf049f4e42c2221447
mkojima <kaekasui_github@icloud.com>
2020-03-06 06:37:40 +0900

    Add api to create and update breakdown

--- server/app/controllers/breakdowns_controller.rb
+++ server/app/controllers/breakdowns_controller.rb
@@ -8,4 +8,28 @@ class BreakdownsController < ApplicationController
       current_user.breakdowns.includes(:category).order(created_at: :desc)
     render json: breakdowns, include: :category, status: :ok
   end
+
+  def create
+    breakdown = current_user.breakdowns.new(breakdown_params)
+    if breakdown.save
+      render json: breakdown, include: :category, status: :created
+    else
+      render_validation_error breakdown
+    end
+  end
+
+  def update
+    breakdown = current_user.breakdowns.find(params[:id])
+    if breakdown.update_attributes(breakdown_params)
+      render json: breakdown, include: :category, status: :ok
+    else
+      render_validation_error breakdown
+    end
+  end
+
+  private
+
+  def breakdown_params
+    params.permit(:name, :category_id)
+  end
 end
