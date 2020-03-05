ec63c4881c61b6d4b6cd1fa573e4833323307e1c
mkojima <kaekasui_github@icloud.com>
2020-03-06 03:51:30 +0900

    Fix and Fix specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   describe 'relationship' do
     it { is_expected.to have_many(:categories).dependent(:destroy) }
     it { is_expected.to have_many(:places).dependent(:destroy) }
-    it { is_expected.to have_many(:breakdowns).through(:categories) }
+    it { is_expected.to have_many(:breakdowns).dependent(:destroy) }
     it { is_expected.to have_many(:templates).through(:categories) }
     it { is_expected.to have_many(:tags).dependent(:destroy) }
     it { is_expected.to have_many(:records).dependent(:destroy) }
