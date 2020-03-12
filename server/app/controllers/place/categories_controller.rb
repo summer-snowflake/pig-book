9169e76f1ac1cdb22cfca2adb73d6b70e7c66d6c
mkojima <kaekasui_github@icloud.com>
2020-03-13 07:46:43 +0900

    Add api to create categorized_places records

--- server/app/controllers/place/categories_controller.rb
+++ server/app/controllers/place/categories_controller.rb
@@ -7,4 +7,16 @@ class Place::CategoriesController < ApplicationController
     place = current_user.places.find(params[:place_id])
     render json: place.categories, status: :ok
   end
+
+  def create
+    place = current_user.places.find(params[:place_id])
+    place.categorized_places.delete_all
+    CategorizedPlace.transaction do
+      params[:category_ids].each do |category_id|
+        place.categorized_places.create!(category_id: category_id)
+      end
+    end
+
+    render json: place.categories, status: :ok
+  end
 end
