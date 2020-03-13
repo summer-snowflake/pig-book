9714de2423ca9c62bc4baab824451ffc1018188a
mkojima <kaekasui_github@icloud.com>
2020-03-13 10:18:58 +0900

    Add input page

---
+++ client/src/components/input/recordForm.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RecordForm extends Component {
+  render(): JSX.Element {
+    return (
+      <div className='record-form-component' />
+    )
+  }
+}
+
+export default RecordForm
---
+++ client/src/components/input/recordsOnInput.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RecordsOnInput extends Component {
+  render(): JSX.Element {
+    return (
+      <div className='records-on-input-component col' />
+    )
+  }
+}
+
+export default RecordsOnInput
