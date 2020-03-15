5b680990347be123689f7d944146eec6dec62bb3
mkojima <kaekasui_github@icloud.com>
2020-03-15 15:07:05 +0900

    Copy record

--- client/src/components/record/records.tsx
+++ client/src/components/record/records.tsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
 
-import { ReadRecord } from 'types/api'
+import { ReadRecord, Record } from 'types/api'
 import RecordTableRecord from 'components/record/recordTableRecord'
 
 interface Props {
   records: ReadRecord[];
+  onClickCopy: (record: Record) => void;
 }
 
 class Records extends Component<Props> {
+  constructor(props: Props) {
+    super(props)
+
+    this.handleClickCopy = this.handleClickCopy.bind(this)
+  }
+
+  handleClickCopy(record: Record): void {
+    this.props.onClickCopy(record)
+  }
+
   render(): JSX.Element {
     return (
       <table className='records-component table'>
         <tbody>
           {this.props.records.map((record) => (
-            <RecordTableRecord key={record.id} record={record} />
+            <RecordTableRecord key={record.id} onClickCopy={this.handleClickCopy} record={record} />
           ))}
         </tbody>
       </table>
