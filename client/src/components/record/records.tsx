46b3d56fedd94046bf15e90bed4b3c774031b857
mkojima <kaekasui_github@icloud.com>
2020-03-23 00:11:29 +0900

    Sort by target column

--- client/src/components/record/records.tsx
+++ client/src/components/record/records.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 import { ReadRecord, Record, Category, Breakdown, Place } from 'types/api'
+import { RecordSearchStore } from 'types/store'
 import RecordTableRecord from 'components/record/recordTableRecord'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface Props {
   records: ReadRecord[];
+  recordSearchStore?: RecordSearchStore;
   editedRecordId: number | undefined;
   format?: string;
   onClickCopy: (record: Record) => void;
@@ -13,6 +16,7 @@ interface Props {
   onClickCategory?: (category: Category) => void;
   onClickBreakdown?: (breakdown: Breakdown) => void;
   onClickPlace?: (place: Place) => void;
+  onClickSort?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
 class Records extends Component<Props> {
@@ -34,6 +38,28 @@ class Records extends Component<Props> {
   render(): JSX.Element {
     return (
       <table className='records-component table'>
+        {this.props.recordSearchStore && this.props.onClickSort && this.props.format === 'detail' && (
+          <tbody>
+            <tr>
+              <td className='icon-field' colSpan={2} data-order='created_at' onClick={this.props.onClickSort}>
+                <FontAwesomeIcon className={(this.props.recordSearchStore.order === 'created_at' ? ' checked' : '')} icon={['fas', 'sort-down']} />
+              </td>
+              <td className='icon-field' data-order='published_at' onClick={this.props.onClickSort}>
+                <FontAwesomeIcon className={(this.props.recordSearchStore.order === 'published_at' ? ' checked' : '')} icon={['fas', 'sort-down']} />
+              </td>
+              <td className='icon-field' data-order='category_id' onClick={this.props.onClickSort}>
+                <FontAwesomeIcon className={(this.props.recordSearchStore.order === 'category_id' ? ' checked' : '')} icon={['fas', 'sort-down']} />
+              </td>
+              <td className='icon-field' data-order='breakdown_id' onClick={this.props.onClickSort}>
+                <FontAwesomeIcon className={(this.props.recordSearchStore.order === 'breakdown_id' ? ' checked' : '')} icon={['fas', 'sort-down']} />
+              </td>
+              <td className='icon-field' data-order='place_id' onClick={this.props.onClickSort}>
+                <FontAwesomeIcon className={(this.props.recordSearchStore.order === 'place_id' ? ' checked' : '')} icon={['fas', 'sort-down']} />
+              </td>
+              <td colSpan={5} />
+            </tr>
+          </tbody>
+        )}
         {this.props.records.map((record) => (
           <RecordTableRecord
             editedRecordId={this.props.editedRecordId}
