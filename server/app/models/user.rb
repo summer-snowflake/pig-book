fcb10396cf0d6eae89cd50a0d3f4e39ebb48cc2f
mkojima <kaekasui_github@icloud.com>
2020-04-15 05:13:22 +0900

    Add confirmable option of devise

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -2,9 +2,10 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
   include DeviseTokenAuth::Concerns::User
 
   has_one :admin, dependent: :destroy
