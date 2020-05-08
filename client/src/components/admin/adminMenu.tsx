e16ea4f27e39b8726206f66210e506c8d9a99465
mkojima <kaekasui_github@icloud.com>
2020-05-08 23:24:27 +0900

    Add send grid link on admin menu

--- client/src/components/admin/adminMenu.tsx
+++ client/src/components/admin/adminMenu.tsx
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom'
 import 'stylesheets/admin.sass'
 
 class AdminMenu extends Component {
+
   render(): JSX.Element {
+    const sendgridUrl = 'https://addons-sso.heroku.com/apps/8ce425c2-9bcb-4ec9-b082-792f0e6596f9/addons/9bad1620-6984-4f37-83bc-f24f5c6e3246'
     return (
       <div className='admin-menu-component'>
         <ul className='nav nav-pills mr-auto'>
@@ -12,6 +14,11 @@ class AdminMenu extends Component {
             <NavLink activeClassName='active' className='nav-link' to='/admin/users'>
               <i className='fas fa-user' />
             </NavLink>
+          </li>
+          <li>
+            <a className='nav-link' href={sendgridUrl} rel='noopener noreferrer' target='_blank'>
+              <i className='fas fa-mail-bulk' />
+            </a>
           </li>
         </ul>
       </div>
