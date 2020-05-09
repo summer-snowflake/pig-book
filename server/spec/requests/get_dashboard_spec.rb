598a01f820610e206736106e623a47ddce2f12f0
mkojima <kaekasui_github@icloud.com>
2020-05-09 20:50:28 +0900

    Add spec for dashboard of monthly

--- server/spec/requests/get_dashboard_spec.rb
+++ server/spec/requests/get_dashboard_spec.rb
@@ -39,6 +39,38 @@ describe 'GET /api/dashboards/:year', autodoc: true do
       end
     end
 
+    context 'there are tally monthly data' do
+      let!(:tally_event) { create(:tally_event, user: user, year: year) }
+      let!(:monthly_balance_table) do
+        create(:monthly_balance_table, user: user)
+      end
+
+      it 'returns status code 200 and json dashboard data' do
+        get "/api/dashboards/#{year}", headers: login_headers_with_login(user)
+
+        expect(response.status).to eq 200
+        json = {
+          event: {
+            user_id: user.id,
+            year: year
+          },
+          monthly: [
+            user_id: user.id,
+            year: monthly_balance_table.year,
+            month: monthly_balance_table.month,
+            expenditure: monthly_balance_table.expenditure,
+            income: monthly_balance_table.income,
+            currency: 'yen',
+            cashless_charge: 0,
+            point: 0
+          ],
+          yearly: nil,
+          year: year
+        }.to_json
+        expect(response.body).to be_json_eql(json)
+      end
+    end
+
     context 'there is NOT tally event' do
       it 'returns status code 200 and json dashboard data' do
         get "/api/dashboards/#{year}", headers: login_headers_with_login(user)
