dfa83f62809270e3852a2abbc3f41c0258e51ebb
mkojima <kaekasui_github@icloud.com>
2020-05-27 13:37:05 +0900

    Fix the axios setting

--- client/src/config/axios.tsx
+++ client/src/config/axios.tsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT
 axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 axios.defaults.baseURL = 'http://localhost:' + SERVER_PORT
+axios.defaults.withCredentials = true
 
 export const setting = axios
