77b646e33229e984bf0d2d1e1f26b53dd723c614
mkojima <kaekasui_github@icloud.com>
2020-05-30 10:44:47 +0900

    Add to notify to slack when exception errors

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  rescue_from Exception, with: :render_exception_error
   rescue_from ActiveRecord::RecordNotFound,
               ActionController::RoutingError,
               with: :render_not_found_error
@@ -15,5 +16,23 @@ class ApplicationController < ActionController::API
   def render_not_found_error
     render json: { erorrs: [I18n.t('errors.not_found')] },
            status: :not_found
+  end
+
+  def render_exception_error(err)
+    notify_to_slack(err)
+    render :system_error, status: 500
+  end
+
+  private
+
+  def notify_to_slack(err)
+    url = "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+    notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
+    attachments = {
+      pretext: "*500 #{err}*\nurl: #{url}\nuser_email: #{current_user&.email}",
+      text: "```\n" + [*err.backtrace].take(25).join("\n") + "\n```",
+      mrkdwn: true
+    }
+    notifier.post attachments
   end
 end
