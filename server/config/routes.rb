4ca2bb42392a22126cf3de5df28dfa85925769b0
mkojima <kaekasui_github@icloud.com>
2020-06-06 02:32:05 +0900

    Add api to get tagged records

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
     resources :places, only: %i[index create update destroy] do
       resources :categories, only: %i[index create], module: :place
     end
-    resources :records, only: %i[index create update destroy]
+    resources :records, only: %i[index create update destroy] do
+      resources :tagged_records, only: %i[index]
+    end
     resources :dashboards, param: :year, only: %i[index show update]
     resource :tutorial, only: %i[show]
 
