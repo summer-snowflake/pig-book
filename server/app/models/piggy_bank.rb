d70c55eed96527d75a49f7ad6aa1ebb8891af83a
mkojima <kaekasui_github@icloud.com>
2021-02-10 18:54:51 +0900

    Add api to get piggy_banks and piggy_bank, or to post and patch a piggy_bank

--- server/app/models/piggy_bank.rb
+++ server/app/models/piggy_bank.rb
@@ -2,11 +2,12 @@
 
 class PiggyBank < ApplicationRecord
   belongs_to :user
+  has_many :piggy_items, dependent: :restrict_with_exception
 
   TITLE_MAX_LENGTH = 30
   DESCRIPTION_MAX_LENGTH = 255
 
   validates :title, presence: true, length: { maximum: TITLE_MAX_LENGTH },
                     uniqueness: { scope: :user }
-  validates :description, presence: true, length: { maximum: DESCRIPTION_MAX_LENGTH }
+  validates :description, length: { maximum: DESCRIPTION_MAX_LENGTH }
 end
--- server/spec/models/piggy_bank_spec.rb
+++ server/spec/models/piggy_bank_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 
 RSpec.describe PiggyBank, type: :model do
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:piggy_items).dependent(:restrict_with_exception) }
 
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_length_of(:title).is_at_most(30) }
-  it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_length_of(:description).is_at_most(255) }
 end
