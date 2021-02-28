a86082ff4da48885aaa2fbcd6ec7120c6c848d99
mkojima <kaekasui_github@icloud.com>
2021-02-28 19:37:15 +0900

    Sort by published_on of piggy_items

--- server/spec/requests/get_piggy_items_spec.rb
+++ server/spec/requests/get_piggy_items_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 describe 'GET /api/piggy_items', autodoc: true do
   let!(:user) { create(:user, :active) }
   let!(:piggy_bank) { create(:piggy_bank, user: user, created_at: 2.hours.ago) }
-  let!(:piggy_item1) { create(:piggy_item, piggy_bank: piggy_bank, name: '内訳1') }
-  let!(:piggy_item2) { create(:piggy_item, piggy_bank: piggy_bank, name: '内訳2') }
+  let!(:piggy_item1) { create(:piggy_item, piggy_bank: piggy_bank, name: '内訳1', published_on: Time.zone.today) }
+  let!(:piggy_item2) { create(:piggy_item, piggy_bank: piggy_bank, name: '内訳2', published_on: Time.zone.yesterday) }
   let!(:path) { "/api/piggy_banks/#{piggy_bank.id}/piggy_items" }
 
   context 'when NOT logged in.' do
@@ -29,16 +29,16 @@ describe 'GET /api/piggy_items', autodoc: true do
       json = [
         {
           piggy_bank_id: piggy_bank.id,
-          balance_of_payments: piggy_item1.balance_of_payments,
-          name: piggy_item1.name,
-          charge: piggy_item1.charge,
-          published_on: Time.zone.today
-        },
-        {
-          piggy_bank_id: piggy_bank.id,
           balance_of_payments: piggy_item2.balance_of_payments,
           name: piggy_item2.name,
           charge: piggy_item2.charge,
+          published_on: Time.zone.yesterday
+        },
+        {
+          piggy_bank_id: piggy_bank.id,
+          balance_of_payments: piggy_item1.balance_of_payments,
+          name: piggy_item1.name,
+          charge: piggy_item1.charge,
           published_on: Time.zone.today
         }
       ].to_json
