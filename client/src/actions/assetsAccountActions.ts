4359ef7df8990ac45251c27c50e459cb7093bca8
mkojima <kaekasui_github@icloud.com>
2021-03-16 15:59:17 +0900

    Fix to devide the actions for assetsAccount

--- client/src/actions/assetsAccountActions.ts
+++ client/src/actions/assetsAccountActions.ts
@@ -9,22 +9,6 @@ import { AssetsAccount, AssetsAccountParams, Errors } from 'types/api'
 import { catchErrors } from 'actions/errorsAction'
 import { getCookiesFailure } from 'actions/userActions'
 
-interface WithBalanceOfPaymentsAction extends Action {
-  balance_of_payments: boolean;
-}
-
-interface WithNameAction extends Action {
-  name: string;
-}
-
-interface WithMoneyAction extends Action {
-  money: string;
-}
-
-interface WithCurrencyAction extends Action {
-  currency: string;
-}
-
 const postAssetsAccountRequest = (): Action => {
   return {
     type: actionTypes.POST_ASSETS_ACCOUNT_REQUEST
@@ -146,52 +130,5 @@ export const deleteAssetsAccount = (assetsAccountId: number) => {
         dispatch(catchErrors(err.response))
       }
     }
-  }
-}
-
-export const changeAssetsAccountBalanceOfPayments = (balance_of_payments: boolean): WithBalanceOfPaymentsAction => {
-  return {
-    type: actionTypes.CHANGE_ASSETS_ACCOUNT_BALANCE_OF_PAYMENTS,
-    balance_of_payments
-  }
-}
-
-export const changeAssetsAccountName = (name: string): WithNameAction => {
-  return {
-    type: actionTypes.CHANGE_ASSETS_ACCOUNT_NAME,
-    name
-  }
-}
-
-export const changeAssetsAccountMoney = (money: string): WithMoneyAction => {
-  return {
-    type: actionTypes.CHANGE_ASSETS_ACCOUNT_MONEY,
-    money
-  }
-}
-
-export const openNewAssetsAccountModal = (currency: string): WithCurrencyAction => {
-  return {
-    type: actionTypes.OPEN_NEW_ASSETS_ACCOUNT_MODAL,
-    currency
-  }
-}
-
-export const closeNewAssetsAccountModal = (): Action => {
-  return {
-    type: actionTypes.CLOSE_NEW_ASSETS_ACCOUNT_MODAL
-  }
-}
-
-export const openEditAssetsAccountModal = (assetsAccount: AssetsAccount): AssetsAccountAction => {
-  return {
-    type: actionTypes.OPEN_EDIT_ASSETS_ACCOUNT_MODAL,
-    assetsAccount
-  }
-}
-
-export const closeEditAssetsAccountModal = (): Action => {
-  return {
-    type: actionTypes.CLOSE_EDIT_ASSETS_ACCOUNT_MODAL
   }
 }
