defe8acb1bd1e2d3b6a47cc508c9fe9c8494d3bd
mkojima <kaekasui_github@icloud.com>
2021-04-22 12:35:53 +0900

    Add settings for react

--- pig-book/app/javascript/packs/application.js
+++ pig-book/app/javascript/packs/application.js
@@ -10,3 +10,7 @@ import * as ActiveStorage from "@rails/activestorage"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
