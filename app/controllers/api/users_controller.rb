ac7b4385b6f0ccd541433b35580be517ccc549c5
kaekasui <kaekasui_github@icloud.com>
2021-07-01 20:43:16 +0900

    Add profile to user data

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,12 +3,17 @@
 module Api
   class UsersController < Api::BaseController
     def show
-      render json: current_user.to_json(include: :admin, methods: %i[options_list options dashboard_years])
+      render json: current_user,
+             include: %i[profile admin],
+             methods: %i[options_list options dashboard_years],
+             status: :ok
     end
 
     def update
       if current_user.update(user_params)
-        render json: current_user.to_json(include: :admin, methods: %i[options_list options dashboard_years]),
+        render json: current_user,
+               include: %i[profile admin],
+               methods: %i[options_list options dashboard_years],
                status: :ok
       else
         render_validation_error current_user
