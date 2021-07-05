a6071a38d087c9aa8052fbd7b223e9c5eb4b9a43
kaekasui <kaekasui_github@icloud.com>
2021-07-06 06:28:24 +0900

    Fix the controllers for api

--- app/controllers/api/tutorials_controller.rb
+++ app/controllers/api/tutorials_controller.rb
@@ -3,8 +3,8 @@
 module Api
   class TutorialsController < Api::BaseController
     def show
-      @tutorial = current_user.tutorial
-      render json: @tutorial, status: :ok
+      tutorial = current_user.tutorial
+      render json: tutorial, status: :ok
     end
   end
 end
